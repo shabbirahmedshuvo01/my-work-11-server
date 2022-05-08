ec0ad04be6ee28663ab2a0b66ae06fc8895744e0
Sabbir Shuvo <Shabbir Ahmed Shuvo>
2022-05-08 16:29:51 +0600

    data get done

--- index.js
+++ index.js
@@ -6,8 +6,18 @@ const port = process.env.PORT || 5000;
 
 const app = express();
 
-
 // middleware
+
+/**
+ * elevenAssignment
+ * book
+ * 
+ * booksDeal
+ * PpyMwh1bE79WFmgh
+ * 
+*/
+
+
 app.use(cors());
 app.use(express.json());
 
@@ -16,20 +26,33 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.4tiwk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-    const collection = client.db("test").collection("devices");
-    console.log('db Connected')
-    // perform actions on the collection object
-    client.close();
-});
 
 
+async function run() {
+    try {
+        await client.connect();
+        const bookCollection = client.db('elevenAssignment').collection('book');
+
+        app.get('/book', async (req, res) => {
+            const query = {};
+            const cursor = bookCollection.find(query);
+            const books = await cursor.toArray();
+            res.send(books);
+        });
+
+    }
+    finally {
+
+    }
+}
+
+run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
-    res.send('eleven-assignment');
+    res.send('Running book server');
 });
 
 app.listen(port, () => {
-    console.log('Port is running on', port);
+    console.log('listening to port', port);
 })
